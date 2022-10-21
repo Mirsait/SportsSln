df37dcbc2a5579da7521cd3b505ae8a2c215f85b
Mirsait <mavcharenko@gmail.com>
2022-10-21 15:03:19 +0300

    update

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository _repository;
 
-        public IActionResult Index() => View();
+        public HomeController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index() => View(_repository.Products);
 
     }
 
